d47e19fab2ac304be8b3fecdd7674bfd9459d226
ruanqiaohua <ruanqiaohua@hotmail.com>
2015-12-15 22:16:28 +0800

    Initial Commit

---
+++ Lua/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Lua
+//
+//  Created by ruanqiaohua on 15/12/15.
+//  Copyright © 2015年 ruanqiaohua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+

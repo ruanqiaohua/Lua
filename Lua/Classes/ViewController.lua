b5b37b664da7f06adf5f0fdf332eb298e1443f69
ruanqiaohua <ruanqiaohua@ruanqiaohuadeMacBook.local>
2015-12-18 23:28:57 +0800

    CollectionView
    CollectionView

--- Lua/Classes/ViewController.lua
+++ Lua/Classes/ViewController.lua
@@ -1,7 +1,22 @@
-waxClass{"ViewController", UIViewController, protocols = {"UITableViewDataSource", "UITableViewDelegate"}}
+waxClass{"ViewController", UIViewController, protocols = {"UICollectionViewDataSource","UICollectionViewDelegate","UICollectionViewDelegateFlowLayout"}}
+
 
 function viewDidLoad(self)
-local background = UIImageView:initWithFrame(self:view():bounds())
-background:setImage(UIImage:imageNamed("tiegui.jpg"))
-self:view():addSubview(background)
+	self.collectionView = UICollectionView:initWithFrame_collectionViewLayout(self:view():bounds(),UICollectionViewFlowLayout:init())
+	self.collectionView:setDelegate(self)
+	self.collectionView:setDataSource(self)
+	self.collectionView:registerClass_forCellWithReuseIdentifier(UICollectionViewCell:class(),"cellIdentifier")
+    self:view():addSubview(self.collectionView)
+
+end
+
+function collectionView_numberOfItemsInSection(self, collectionView, section)
+	return 100
+end
+
+function collectionView_cellForItemAtIndexPath(self, collectionView, indexPath)
+	local cell = collectionView:dequeueReusableCellWithReuseIdentifier_forIndexPath("cellIdentifier", indexPath)
+	cell:setBackgroundColor(UIColor:yellowColor())
+
+    return cell
 end
